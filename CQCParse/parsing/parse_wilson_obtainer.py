42fa8d4db5478208150ab9f3eb64da1c67065980
Vanda Le <vandwlev@gmail.com>
2026-01-17 23:24:58 +0100

    upd nc_sqrt_eigval parsed data

--- CQCParse/parsing/parse_wilson_obtainer.py
+++ CQCParse/parsing/parse_wilson_obtainer.py
@@ -189,7 +189,8 @@ results {                       'Fundamental Bands': [['Fundamental', 'Bands'], 
                 len_nmodes = len(fund_harm_dict)
 
                 if 'nc_sqrt_eigval' in requested_data:
-                    results['nc_sqrt_eigval'] = fund_harm_dict
+                    print(fund_harm_dict)
+                    results['nc_sqrt_eigval'] = {int(k[0]): v for k,v in fund_harm_dict.items()}
             except Exception as e:
                 print('Failed at "harmonic_states/nc_sqrt_eigval" with:', e)
                 raise ValueError("smth went wrong")
